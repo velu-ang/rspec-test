8275dbb20f378bf8bc4d56ed3bf00af8a375113b
velusamy <velusamy@angleritech.com>
2018-05-08 13:44:01 +0530

    first test commit

---
+++ db/migrate/20180508070502_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.string :photo
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
